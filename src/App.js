d95397a20e6623b759d06b77b4eb2fc2085f65c9
Manjulanenavath <manjula.nenavath@outlook.com>
2021-03-17 11:36:57 +1300

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from './Menu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quiz App</h1>
+      <hr/>
+      <Menu/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function Menu(){
+
+    return(
+        <div className="menu">
+            <input type="text" placeholder="Enter your Name"/ >
+            <button>Submit</button>
+        </div>
+    );
+}
+export default Menu;
